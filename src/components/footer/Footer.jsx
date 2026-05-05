d368bbc4abfa8c20e38ecd2a69a6e0b00542989d
abdulrahman <abdelrahmanabuharag@gmail.com>
2026-05-05 15:00:30 +0300

    update href

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
             </div>
             <div className="footer-bottom">
                 &copy; {new Date().getFullYear()} Arizona Bakery. All rights reserved.
-                Powered by <a href="https://www.emereld-marketing.online">Adam's Agency</a>
+                Powered by <a href="http://adams-agency.online">Adam's Agency</a>
             </div>
         </footer>
     )
